40236d7d1b1dc450f3ecb56d5db56a31a4acdcac
Aslam <aralex1016@gmail.com>
2025-07-03 00:59:33 +0300

    Add Footer Animation

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,13 +1,38 @@
+import { useRef } from "react";
+import { animate, motion, useInView } from "framer-motion";
+
 // Components
 import SocialMedia from "../SocialMedia";
 import BackToTop from "../BackToTop";
 
 const Footer = () => {
+  const footerRef = useRef(null);
+  const isInView = useInView(footerRef, {
+    once: true,
+  });
+
   const name = import.meta.env.VITE_API_NAME;
   const currentYear = new Date().getFullYear();
 
   return (
-    <section className="w-full flex flex-col justify-center items-center gap-y-2 pb-2">
+    <motion.section
+      ref={footerRef}
+      variants={{
+        initial: {
+          x: -300,
+        },
+        animate: {
+          x: 0,
+        },
+      }}
+      initial="initial"
+      animate={isInView ? "animate" : "initial"}
+      transition={{
+        duration: 1,
+        ease: "linear",
+      }}
+      className="w-full flex flex-col justify-center items-center gap-y-2 pb-2"
+    >
       <p className="text-white text-sm">
         ©{" "}
         <span className="font-medium">
@@ -19,7 +44,7 @@ const Footer = () => {
       <SocialMedia />
 
       <BackToTop className="rounded-full bg-secondary/30 hover:bg-secondary/50 p-1 absolute right-sidePadding bottom-2 animate-bounce" />
-    </section>
+    </motion.section>
   );
 };
 
