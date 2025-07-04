5b5fe905e79a6432e80fafb116199b6570bacbbf
Aslam <aralex1016@gmail.com>
2025-07-04 12:18:42 +0300

    Added Social Links

--- client/src/Components/SocialMedia.jsx
+++ client/src/Components/SocialMedia.jsx
@@ -10,9 +10,18 @@ const socialMediaStyle = "text-white hover:text-secondary";
 const SocialMedia = ({ className }) => {
   return (
     <div className={`flex flex-row gap-x-2 ${className}`}>
-      <GitHubIconMui className={socialMediaStyle} />
-      <WhatsAppIconMui className={socialMediaStyle} />
-      <FacebookOutlinedIconMui className={socialMediaStyle} />
+      <a href="https://github.com/" target="_blank" className="">
+        <GitHubIconMui className={socialMediaStyle} />
+      </a>
+      <a href="#">
+        <WhatsAppIconMui className={socialMediaStyle} />
+      </a>
+      <a
+        href="https://www.facebook.com/profile.php?id=100015751629762"
+        target="_blank"
+      >
+        <FacebookOutlinedIconMui className={socialMediaStyle} />
+      </a>
     </div>
   );
 };
